6967db83f511b7385a39f871d261e8665a4fe837
jaimein <>
2016-01-19 18:49:18 +0100

    index realizado

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Proyecto CRUD</title>
+        <link media="all" href="css/style.css" rel="stylesheet" type="text/css"></link>
+        </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <div id="logo">  
+                    <img src="img/logo_blanco_0.png"></img>
+                </div>
+                <div id="title">
+                    Examen-ASIR-project
+                </div>
+            </div>
+            <div id="content">
+                <?php
+                include 'controlador.php'; //Ahora mostramos la pagina llamada
+                ?>
+            </div>
+            <div id="footer">
+                <div id="subtitle">  
+                    <a href="http://www.ausiasmarch.net/asir"> CFGS Administración de Sistemas Informáticos y Redes </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
