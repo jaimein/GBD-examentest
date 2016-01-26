dc3694a064fd6ce2c8ca0317cf49536b2b281bf0
Jaimein <Juanjo@Juanjo-PC>
2016-01-26 09:11:01 +0100

    Modificacion de lista para que muestre los examenes

--- lista.php
+++ lista.php
@@ -1,23 +1,20 @@
-        <h1>Listado de Clientes</h1>
+        <h1>Listado de Examenes</h1>
         <?php
 // incluir la conexión a la base de datos
  //       include 'conexion.php';
         $estado = isset($_GET['estado']) ? $_GET['estado'] : "";
 
-// si viene de borra.php
-        if ($estado == 'deleted') {
-            echo "<div>El registro cliente ha sido borrado.</div>";
-        }
 // Elegir los datos que deseamos recuperar de la tabla
-        $query = "SELECT id, nif, nombre, apellido1, apellido2, email, telefono, usuario  "
-                . "FROM clientes "
-                . "ORDER BY apellido1, apellido2, nombre";
+        $query = "SELECT e.id AS ExamenId, e.descripcion as Asignatura, c.descripcion as Curso "
+                . "FROM examen e, cursos c "
+                . "WHERE e.cursos_id=c.id ";
+       // echo $query;
         if ($stmt = $conexion->prepare($query)) {
             if (!$stmt->execute()) {
                 die('Error de ejecución de la consulta. ' . $conexion->error);
             }
 // recoger los datos
-            $stmt->bind_result($id, $nif, $nombre, $apellido1, $apellido2, $email, $telefono, $usuario);
+            $stmt->bind_result($ExamenId, $Asignatura, $Curso);
 
 // enlace a alta de cliente
             echo "<div>";
@@ -28,30 +25,20 @@
             echo "<table>"; //start table
             //creating our table heading
             echo "<tr>";
-            echo "<th>NIF</th>";
-            echo "<th>Nombre</th>";
-            echo "<th>Apellido 1</th>";
-            echo "<th>Apellido 2</th>";
-            echo "<th>email</th>";
-            echo "<th>telefono</th>";
-            echo "<th>usuario</th>";
+            echo "<th>Curso</th>";
+            echo "<th>Asignatura</th>";
             echo "</tr>";
 //recorrido por el resultado de la consulta
             while ($stmt->fetch()) {
                 echo "<tr>";
-                echo "<td>$nif</td>";
-                echo "<td>$nombre</td>";
-                echo "<td>$apellido1</td>";
-                echo "<td>$apellido2</td>";
-                echo "<td>$email</td>";
-                echo "<td>$telefono</td>";
-                echo "<td>$usuario</td>";
+                echo "<td>$Curso</td>";
+                echo "<td>$Asignatura</td>";
                 echo "<td>";
 // Este enlace es para modificar el registro
-                echo "<a href='index.php?accion=edita&id={$id}'>Edita</a>";
+                echo "<a href='index.php?accion=hacer&id={$ExamenId}'>Realizar examen</a>";
                 echo " / ";
 // Este enlace es para borrar el registro y también se explicará más tarde
-                echo "<a href='javascript:borra_cliente(\"$id\")'> Elimina </a>";
+//                echo "<a href='javascript:borra_cliente(\"$id\")'> Elimina </a>";
                 echo "</td>";
                 echo "</tr>\n";
             }
